1d11b756ffd457331dcdffdab9f463f799bf1b74
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-09-29 08:27:38 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-gh_repo="papirus-icon-theme"
-gh_desc="Papirus icon theme"
+gh_repo="wow64-icon-theme"
+gh_desc="wow64 icon theme"
 
 cat <<- EOF
 
@@ -19,13 +19,13 @@ cat <<- EOF
 
 
   $gh_desc
-  https://github.com/PapirusDevelopmentTeam/$gh_repo
+  https://github.com/luisrguerra/$gh_repo
 
 
 EOF
 
 echo "=> Removing $gh_desc ..."
-for i in ePapirus ePapirus-Dark Papirus Papirus-Adapta Papirus-Adapta-Nokto Papirus-Dark Papirus-Light; do
+for i in ewow64 ewow64-Dark wow64 wow64-Adapta wow64-Adapta-Nokto wow64-Dark wow64-Light; do
   rm -rf "$HOME/.icons/$i"
   rm -rf "$HOME/.local/share/icons/$i"
   sudo rm -rf "/usr/local/share/icons/$i"
