9aef49b9dd6e8c300557c1895592a5a094079932
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-10-08 11:33:24 -0300

    scripts suport to light theme

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@ echo Theme installation
 echo removing possible old theme files
 
 sudo rm -R /usr/share/icons/wow64
+sudo rm -R /usr/share/icons/wow64-Light
 
 echo Installing theme at /usr/share/icons
 
 sudo cp -fr wow64 /usr/share/icons
+sudo cp -fr wow64-Light /usr/share/icons
 
 echo creating icon theme cache
 
 sudo gtk-update-icon-cache -q /usr/share/icons/wow64
+sudo gtk-update-icon-cache -q /usr/share/icons/wow64-Light
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,3 @@
 echo removing theme files
 sudo rm -R /usr/share/icons/wow64
+sudo rm -R /usr/share/icons/wow64-Light
