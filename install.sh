e9a0a573177f534ffc5b18ba96b60f38b1327dca
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-10-28 08:28:44 -0300

    Ascii art on installation fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
+cat << "EOF"
+ ____
+|____\_________
+|              |
+|              |
+|    ______    | Wow64
+|   / ____ \   | Icon Theme
+|___|______|___|
+
+EOF
+
 echo Theme installation
 
-echo removing possible old theme files
+echo Removing possible old theme files
 
 sudo rm -R /usr/share/icons/wow64
 sudo rm -R /usr/share/icons/wow64-Light
@@ -10,7 +21,7 @@ echo Installing theme at /usr/share/icons
 sudo cp -fr wow64 /usr/share/icons
 sudo cp -fr wow64-Light /usr/share/icons
 
-echo creating icon theme cache
+echo Creating icon theme cache
 
 sudo gtk-update-icon-cache -q /usr/share/icons/wow64
 sudo gtk-update-icon-cache -q /usr/share/icons/wow64-Light
