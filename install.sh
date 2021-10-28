6d89b3a7a00f594a5947477c862756c4ce5b7189
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2021-10-28 15:29:00 -0300

    install improvement

--- install.sh
+++ install.sh
@@ -11,13 +11,32 @@ EOF
 
 echo Theme installation
 
-echo Removing possible old theme files
 
-sudo rm -R /usr/share/icons/wow64
-sudo rm -R /usr/share/icons/wow64-Light
+if [ -e ~/.icons/wow64 ]
+then
+   echo wow64 installation found at home folder, uninstalling...
+   sudo rm -R ~/.icons/wow64
+fi
 
-sudo rm -R ~/.icons/wow64
-sudo rm -R ~/.icons/wow64-Light
+if [ -e ~/.icons/wow64-Light ]
+then
+   echo wow64-Light installation found at home folder, uninstalling...
+   sudo rm -R ~/.icons/wow64-Light
+fi
+
+if [ -e /usr/share/icons/wow64 ]
+then
+   echo wow64 installation found at system icon themes, uninstalling...
+   sudo rm -R /usr/share/icons/wow64
+fi
+
+if [ -e /usr/share/icons/wow64-Light ]
+then
+   echo wow64-Light installation found at system icon themes, uninstalling...
+   sudo rm -R /usr/share/icons/wow64-Light
+fi
+
+
 
 echo Installing theme at /usr/share/icons
 
