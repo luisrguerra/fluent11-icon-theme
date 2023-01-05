496d03a14ffa947214bec99bb69e16aa21489977
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2023-01-05 08:57:54 -0300

    rename

--- dev-tools/copy-scalable-apps.bat
+++ dev-tools/copy-scalable-apps.bat
@@ -1,9 +1,9 @@
-SET folder16="../wow64/16x16/apps"
-SET folder22="../wow64/22x22/apps"
-SET folder24="../wow64/24x24/apps"
-SET folder32="../wow64/32x32/apps"
-SET folder48="../wow64/48x48/apps"
-SET folder64="../wow64/64x64/apps"
+SET folder16="../fluent11/16x16/apps"
+SET folder22="../fluent11/22x22/apps"
+SET folder24="../fluent11/24x24/apps"
+SET folder32="../fluent11/32x32/apps"
+SET folder48="../fluent11/48x48/apps"
+SET folder64="../fluent11/64x64/apps"
 xcopy /i /e /y "scalable" %folder16%
 xcopy /i /e /y "scalable" %folder22%
 xcopy /i /e /y "scalable" %folder24%
